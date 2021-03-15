abc26481539630f236febf57765b47ba3b2dd709
Steven Nguyen <steeven.nguyen1102@gmail.com>
2021-03-15 15:40:50 -0700

    create ship array for computer ships

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Playergrid from './Playergrid.jsx';
 import Computergrid from './Computergrid.jsx';
 import Display from './Display.jsx';
 import './App.scss';
 
+const createBoard = (squares) => {
+    for (let i = 0; i < 100; i++) {
+        squares.push(i);
+    }
+    return squares;
+}
+
 const App = () => {
+    const [width, setWidth] = useState(10);
+
     return (
         <div className="container">
-            <Playergrid />
-            <Computergrid />
+            <Playergrid createBoard={createBoard} width={width}/>
+            <Computergrid createBoard={createBoard} width={width}/>
             <Display />
         </div>
     )
--- client/components/Playergrid.jsx
+++ client/components/Playergrid.jsx
@@ -1,15 +1,8 @@
 import React, { useState } from 'react';
 import './Playergrid.scss';
 
-const createBoard = (squares) => {
-    for (let i = 0; i < 100; i++) {
-        squares.push(i);
-    }
-    return squares;
-}
-
-const Playergrid = () => {
-    const [squares, setSquares] = useState(createBoard([]));
+const Playergrid = (props) => {
+    const [squares, setSquares] = useState(props.createBoard([]));
 
     return (
         <div className="grid grid-user">
