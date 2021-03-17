5ff6bf7754aa33a8fb61441129e8819223da0299
Steven Nguyen <steeven.nguyen1102@gmail.com>
2021-03-16 18:36:32 -0700

    finish math of calculating each x and y of each grid tile to start snapping pieces once dropped

--- client/components/Chessboard.jsx
+++ client/components/Chessboard.jsx
@@ -1,48 +1,52 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import './Chessboard.scss';
 import Tile from './Tile.jsx';
-
-const verticalAxis = ['1', '2', '3', '4', '5', '6', '7', '8'];
-const horizontalAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-const pieces = [];
-
-for (let i = 0; i < 8; i++) {
-    pieces.push({
-        image: 'assets/pawn_b.png',
-        x: i,
-        y: 6
-    })
-}
-
-for (let i = 0; i < 8; i++) {
-    pieces.push({
-        image: 'assets/pawn_w.png',
-        x: i,
-        y: 1
-    })
-}
-
-for (let p = 0; p < 2; p++) {
-    const type = p === 0 ? 'b' : 'w';
-    const y = p === 0 ? 7 : 0;
-
-    pieces.push({ image: `assets/rook_${type}.png`, x: 0, y: y});
-    pieces.push({ image: `assets/rook_${type}.png`, x: 7, y: y});
-    pieces.push({ image: `assets/knight_${type}.png`, x: 1, y: y});
-    pieces.push({ image: `assets/knight_${type}.png`, x: 6, y: y});
-    pieces.push({ image: `assets/bishop_${type}.png`, x: 2, y: y});
-    pieces.push({ image: `assets/bishop_${type}.png`, x: 5, y: y});
-    pieces.push({ image: `assets/king_${type}.png`, x: 4, y: y});
-    pieces.push({ image: `assets/queen_${type}.png`, x: 3, y: y});
-    
-}
-
 
 
 export default function Chessboard() {
     const chessboardRef = useRef(null);
+    const [pieces, setPieces] = useState([]);
     
     let activePiece = null;
+    
+    useEffect(() => {
+        const verticalAxis = ['1', '2', '3', '4', '5', '6', '7', '8'];
+        const horizontalAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+        const pieces = [];
+        
+        for (let i = 0; i < 8; i++) {
+            pieces.push({
+                image: 'assets/pawn_b.png',
+                x: i,
+                y: 6
+            })
+        }
+        
+        for (let i = 0; i < 8; i++) {
+            pieces.push({
+                image: 'assets/pawn_w.png',
+                x: i,
+                y: 1
+            })
+        }
+        
+        for (let p = 0; p < 2; p++) {
+            const type = p === 0 ? 'b' : 'w';
+            const y = p === 0 ? 7 : 0;
+        
+            pieces.push({ image: `assets/rook_${type}.png`, x: 0, y: y});
+            pieces.push({ image: `assets/rook_${type}.png`, x: 7, y: y});
+            pieces.push({ image: `assets/knight_${type}.png`, x: 1, y: y});
+            pieces.push({ image: `assets/knight_${type}.png`, x: 6, y: y});
+            pieces.push({ image: `assets/bishop_${type}.png`, x: 2, y: y});
+            pieces.push({ image: `assets/bishop_${type}.png`, x: 5, y: y});
+            pieces.push({ image: `assets/king_${type}.png`, x: 4, y: y});
+            pieces.push({ image: `assets/queen_${type}.png`, x: 3, y: y});
+            
+        }
+        setPieces(pieces);
+        
+    }, [])
     
     const grabPiece = (e) => {
         if (e.target.className === 'chess-piece') {
@@ -89,15 +93,30 @@ export default function Chessboard() {
     }
     
     const dropPiece = (e) => {
-        if (activePiece) {
+        let chessboard = chessboardRef.current;
+        if (activePiece && chessboard) {
+            // these const make mapping the grid to start at 0, 0 and increment in 1's
+            const x = Math.floor((e.clientX - chessboard.offsetLeft) / 100);
+            const y = Math.floor((e.clientY - chessboard.offsetTop) / 100);
+            console.log(x, y);
+            setPieces((value) => {
+                const pieces = value.map((p) => {
+                    if (p.x == 0 && p.y == 0) {
+                        p.x == 5;
+                    }
+                    return p;
+                });
+                return pieces;
+            })
+
             activePiece = null;
         }
     }
     
     let board = [];
 
-    for (let i = verticalAxis.length - 1; i >= 0; i--) {
-        for (let j = 0; j < horizontalAxis.length; j++) {
+    for (let i = 7; i >= 0; i--) {
+        for (let j = 0; j < 8; j++) {
             const number = j + i + 2;
             let image = undefined;
             pieces.forEach((p) => {
