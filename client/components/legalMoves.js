665e3e289bf4d75ca0643a0f3f1dbb3fee59f2e4
Steven Nguyen <steeven.nguyen1102@gmail.com>
2021-03-17 21:41:29 -0700

    complete basic move logic for black pawns

--- client/components/legalMoves.js
+++ client/components/legalMoves.js
@@ -9,13 +9,26 @@ export default class LegalMoves {
                     if (previousX === x && (y - previousY === 1 || y - previousY === 2)) {
                         console.log('valid')
                         return true;
-                    } else {
-                        console.log('not valid')
-                        return false;
+                    } 
+                } else {
+                    if (previousX === x && y - previousY === 1) {
+                        console.log('valid');
+                        return true;
+                    }
+                }
+            } else {
+                if (previousY === 6) {
+                    if (previousX ===x && ((previousY - y === 1) || Math.abs(previousY - y === 2))) {
+                        console.log('valid');
+                        return true;
+                    }
+                } else {
+                    if (previousX === x && ((previousY - y === 1))) {
+                        return true;
                     }
                 }
             }
         }
-        return true;
+        return false;
     }
 }
