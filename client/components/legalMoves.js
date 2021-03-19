a749d4cdf30b535b7d44f78a33252fdacf218c2b
Steven Nguyen <steeven.nguyen1102@gmail.com>
2021-03-19 01:28:06 -0700

    refactored pawn moving logic

--- client/components/legalMoves.js
+++ client/components/legalMoves.js
@@ -14,43 +14,65 @@ export default class LegalMoves {
 
         // pawn
         if (type === 'pawn') {
-            if (color === 'white') {
-                if (previousY === 1) {
-                    if (previousX === x && (y - previousY === 1)) {
-                        if (!this.isTileOccupied(x, y, stateOfBoard)) {
-                            return true;
-                        }
-                    } else if (previousX === x && (y - previousY === 2)) {
-                        if (!this.isTileOccupied(x, y, stateOfBoard) && !this.isTileOccupied(x, y - 1, stateOfBoard)) {
-                            return true;
-                        }
-                    }
-                } else {
-                    if (previousX === x && y - previousY === 1) {
-                        if (!this.isTileOccupied(x, y, stateOfBoard)) {
-                            return true;
-                        }
+            const startRow = (color === 'white') ? 1 : 6;
+            const pawnDirection = (color === 'white') ? 1 : -1;
+
+            if (previousY === startRow) {
+                if (previousX === x && y - previousY === 1 * pawnDirection) {
+                    if (!this.isTileOccupied(x, y, stateOfBoard)) {
+                        return true;
+                    } 
+                } else if (previousX === x && y - previousY === 2 * pawnDirection) {
+                    if (!this.isTileOccupied(x, y, stateOfBoard)) {
+                        return true;
                     }
                 }
             } else {
-                if (previousY === 6) {
-                    if (previousX ===x && ((previousY - y === 1))) {
-                        if (!this.isTileOccupied(x, y, stateOfBoard)) {
-                            return true;
-                        }
-                    } else if (previousX === x && ((previousY - y === 2))) {
-                        if (!this.isTileOccupied(x, y, stateOfBoard) && !this.isTileOccupied(x, y + 1, stateOfBoard)) {
-                            return true;
-                        }
-                    }
-                } else {
-                    if (previousX === x && ((previousY - y === 1))) {
-                        if (!this.isTileOccupied(x, y, stateOfBoard)) {
-                            return true;
-                        }
+                if (previousX === x && y - previousY === 1 * pawnDirection) {
+                    if (!this.isTileOccupied(x, y, stateOfBoard)) {
+                        return true;
                     }
                 }
             }
+
+
+            // if (color === 'white') {
+            //     if (previousY === 1) {
+            //         if (previousX === x && (y - previousY === 1)) {
+            //             if (!this.isTileOccupied(x, y, stateOfBoard)) {
+            //                 return true;
+            //             }
+            //         } else if (previousX === x && (y - previousY === 2)) {
+            //             if (!this.isTileOccupied(x, y, stateOfBoard) && !this.isTileOccupied(x, y - 1, stateOfBoard)) {
+            //                 return true;
+            //             }
+            //         }
+            //     } else {
+            //         if (previousX === x && y - previousY === 1) {
+            //             if (!this.isTileOccupied(x, y, stateOfBoard)) {
+            //                 return true;
+            //             }
+            //         }
+            //     }
+            // } else {
+            //     if (previousY === 6) {
+            //         if (previousX ===x && ((previousY - y === 1))) {
+            //             if (!this.isTileOccupied(x, y, stateOfBoard)) {
+            //                 return true;
+            //             }
+            //         } else if (previousX === x && ((previousY - y === 2))) {
+            //             if (!this.isTileOccupied(x, y, stateOfBoard) && !this.isTileOccupied(x, y + 1, stateOfBoard)) {
+            //                 return true;
+            //             }
+            //         }
+            //     } else {
+            //         if (previousX === x && ((previousY - y === 1))) {
+            //             if (!this.isTileOccupied(x, y, stateOfBoard)) {
+            //                 return true;
+            //             }
+            //         }
+            //     }
+            // }
         }
         return false;
     }
